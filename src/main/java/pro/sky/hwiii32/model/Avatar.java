3316a43a618403a756434547ed5e444065b15c80
slavzick <slavzick@mail.ru>
2022-11-08 20:43:35 +0500

    HW-III-3.2-lesson_3.5-done-fix

--- src/main/java/pro/sky/hwiii32/model/Avatar.java
+++ src/main/java/pro/sky/hwiii32/model/Avatar.java
@@ -10,6 +10,7 @@ public class Avatar {
     private String filePath;
     private long fileSize;
     private String mediaType;
+    @Lob
     private byte[] data;
     @OneToOne
     @JoinColumn(name = "student_id")
